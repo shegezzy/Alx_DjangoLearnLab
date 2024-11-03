33c9cbe96c65988f48b970b22a6e3b4bf9875684
shegezzy <shegezzy@gmail.com>
2024-11-04 00:00:11 +0200

    Update admin.py

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Book
 
-@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
  # Fields to display in the list view
     list_display = ('title', 'author', 'publication_year')
     
