1b144454769c6821c4dda5bc519db4c0d080c8b3
shegezzy <shegezzy@gmail.com>
2024-11-04 19:35:49 +0200

    Updated superuser

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+    publication_year = models.IntegerField(default=2020)
 
     def __str__(self):
       return self.title
