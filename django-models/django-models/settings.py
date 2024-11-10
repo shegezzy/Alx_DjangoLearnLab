46ed20fe0fb0f77b87f2f070391490cde88a1405
shegezzy <shegezzy@gmail.com>
2024-11-10 20:20:10 +0200

    updated new user profile

--- django-models/django-models/settings.py
+++ django-models/django-models/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'relationship_app'
-    'accounts'
+    
 ]
 
 MIDDLEWARE = [
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'django-models.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
