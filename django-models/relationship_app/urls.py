8fab9de0edacbef4ec4db1600140e944899b5dcc
shegezzy <shegezzy@gmail.com>
2024-11-10 02:13:34 +0200

    added user authentication

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -2,8 +2,12 @@
 
 from django.urls import path
 from .views import list_books, LibraryDetailView
+from .views import user_login, user_logout, register
 
 urlpatterns = [
-    path('books/', list_books, name='list_books'),  # URL for function-based view
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # URL for class-based view
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
+    path('register/', register, name='register'),
+    path('books/', list_books, name='list_books'),  
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,7 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  #Home page or dashboard
+        else:
+            return render(request, 'login.html', {'error': 'Invalid credentials'})
+    return render(request, 'login.html')
+
+def user_logout(request):
+    logout(request)
+    return render(request, 'logout.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')  # Redirect to a success page (home or dashboard)
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
 
 def list_books(request):
     books = Book.objects.all()  # Fetch all books from the database
