d637670ea823205dbe36b6db3e64c3e7b43800d4
shegezzy <shegezzy@gmail.com>
2024-11-15 10:28:25 +0200

    updated permissions

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -4,9 +4,9 @@ from .models import Post
 from .forms import PostForm
 
 @permission_required('bookshelf.can_view', raise_exception=True)
-def post_detail(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    return render(request, 'post_detail.html', {'post': post})
+def book_detail(request, book_id):
+    post = get_object_or_404(Post, id=book_id)
+    return render(request, 'book_detail.html', {'book': post})
 
 @permission_required('bookshelf.can_create', raise_exception=True)
 def post_create(request):
@@ -14,25 +14,25 @@ def post_create(request):
         form = PostForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('post_list')
+            return redirect('book_list')
     
 
 @permission_required('bookshelf.can_edit', raise_exception=True)
-def post_edit(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
+def book_edit(request, book_id):
+    book = get_object_or_404(Post, id=book_id)
     if request.method == 'POST':
-        form = PostForm(request.POST, instance=post)
+        form = PostForm(request.POST, instance=book)
         if form.is_valid():
             form.save()
-            return redirect('post_detail', post_id=post.id)
+            return redirect('book_detail', book_id=book.id)
     else:
-        form = PostForm(instance=post)
+        form = PostForm(instance=book)
     return render(request, 'post_form.html', {'form': form})
 
-@permission_required('myapp.can_delete', raise_exception=True)
-def post_delete(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    post.delete()
-    return redirect('post_list')
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def post_delete(request, book_id):
+    book = get_object_or_404(Post, id=book_id)
+    book.delete()
+    return redirect('book_list')
 
 # Create your views here.
