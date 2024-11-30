64d506b07d46232bd668a96d7ebae6d895c6a121
shegezzy <shegezzy@gmail.com>
2024-11-30 22:16:15 +0200

    updated the test file and CRUD operations

--- advanced-api-project/api/tests.py
+++ advanced-api-project/api/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import Book, Author
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+
+class BookTests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='Power', password='power123')
+        self.token = Token.objects.create(user=self.user)
+        self.author = Author.objects.create(name="Sally Thorne")
+        self.book = Book.objects.create(title="The Hating Game", publication_year=2019, author=self.author)
+
+    def test_create_book(self):
+        url = '/api/books/'
+        data = {'title': 'New Book', 'publication_year': 2024, 'author': self.author.id}
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_book_list(self):
+        url = '/api/books/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
 
 # Create your tests here.
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,20 +1,30 @@
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from rest_framework import generics
+from rest_framework import generics, filters, status
 from .serializers import BookSerializer
 from .models import Book
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
 
 class CustomBookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
 
-    def perform_create(self, serializer):
-        serializer.save()
+    def post(self, request, *args, **kwargs):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class CustomBookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+    filterset_fields = ['title', 'author__name', 'publication_year']
+    ordering_fields = ['title', 'publication_year']
+    search_fields = ['title', 'author__name']
 
 class CustomBookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
