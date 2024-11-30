ccce8abf95e7c7bf1ecdcbefb7f94916e1f3708d
shegezzy <shegezzy@gmail.com>
2024-12-01 01:18:20 +0200

    updated the view file

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,8 @@
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from rest_framework import generics, filters
+from rest_framework import generics
 from .serializers import BookSerializer
 from .models import Book, Author
+from django_filters import rest_framework
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 
