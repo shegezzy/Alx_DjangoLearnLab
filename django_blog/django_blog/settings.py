48cd284ed0b40ccc41595ca6bfc129fcc002e17a
shegezzy <shegezzy@gmail.com>
2024-12-02 12:18:02 +0200

    changed database from sqlite3 to MySql

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,8 +79,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blogpost',
+        'USER': 'root',
+        'PASSWORD': 'MyAlx0912',
+        'HOST': '127.0.0.1',
+        'POST': '3306',
     }
 }
 
