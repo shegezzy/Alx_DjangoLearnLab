4d4a1e7722592e3a59c3cad8bec90657244efff1
shegezzy <shegezzy@gmail.com>
2024-12-03 22:17:16 +0200

    updated user comments

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
-from .views import post_search
+from .views import post_search, CommentCreateView, CommentUpdateView, CommentDeleteView
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
@@ -12,9 +12,12 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('posts/', views.PostListView.as_view(template_name='blog/post_list.html'), name='post-list'),
     path('post/<int:pk>/', views.PostDetailView.as_view(template_name='blog/post_detail.html'), name='post-detail'),
+     path('post/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-edit'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
     path('post/new/', views.PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(template_name='blog/post_confirm_delete.html'), name='post-delete'),
     path('search/', post_search, name='post-search'),
-     path('tags/<str:tag_name>/', post_search, name='tag-detail'),
+    path('tags/<str:tag_name>/', post_search, name='tag-detail'),
 ]
