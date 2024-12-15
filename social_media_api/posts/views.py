6f43fb1f06bea15cc878c25498b4d2aa0eea49fd
shegezzy <shegezzy@gmail.com>
2024-12-15 17:09:44 +0200

    updated the posts/view file

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -187,7 +187,7 @@ class LikePostView(generics.CreateAPIView):
         
     def perform_create(self, serializer):
         post_id = self.kwargs['pk']
-        # post = get_object_or_404(Post, pk=post_id)
+        post = get_object_or_404(Post, pk=post_id)
         post = generics.get_object_or_404(Post, pk=post_id)
         Like.objects.get_or_create(user=self.request.user, post=post)
         if Like.objects.filter(user=self.request.user, post=post).exists():
@@ -227,5 +227,10 @@ class UnlikePostView(generics.DestroyAPIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-
+# def post(self, request):
+    #     pk = self.kwargs['pk']
+    #     # post = get_object_or_404(Post, pk=pk)
+    #     post = generics.get_object_or_404(Post, pk=post_id)
+    #     Like.objects.get_or_create(user=request.user, post=post)
+    #     return Response(status=status.HTTP_201_CREATED)
 # Create your views here.
